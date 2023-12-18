c8a2dc90120fcfbf13192514e72ae76f65c7b8ab
Giordano94 <giordano.mendes94@gmail.com>
2023-12-17 21:55:45 -0300

    styles(AppBar): add styled components for toolbar, search bar, and logo

---
+++ src/Components/AppBar/styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ToolbarWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 16px;
+`;
+
+export const SearchBarWrapper = styled.div`
+  margin: 0 80px 0 80px;
+`;
+
+export const StyledLogo = styled.div`
+  margin: 8px;
+`;
